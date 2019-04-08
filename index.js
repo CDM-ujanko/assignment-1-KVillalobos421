1f64d2dfcfb5902996393ea25564b540c31b71b2
Kevin Villalobos <Kvillalobos421@gmail.com>
2019-04-08 17:14:38 -0500

    add all

---
+++ index.js
@@ -0,0 +1,22 @@
+function display() {
+   alert("Welcome to the page! Take a short look!");
+}
+
+
+console.log('The New File');
+
+var x = 'a string variable';
+console.log(x);
+
+//console.log(document);
+//console.log($(document));
+console.log($('myId p'));
+
+var mydiv = document.getElementById('myId');
+mydiv.setAttribute('style', 'color: blue');
+console.log(mydiv);
+//console.log($);
+//$( document ).ready(function() {
+  // Handler for .ready() called.
+//});
+//console.log('I am Ready to Zipline');
