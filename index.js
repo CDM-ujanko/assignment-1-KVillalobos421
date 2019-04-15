4bdbfa018c779385e923e45b64c25904e7029bfa
Kevin Villalobos <Kvillalobos421@gmail.com>
2019-04-15 15:59:07 -0500

    New edits to the work

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-function display() {
-   alert("Welcome to the page! Take a short look!");
-}
-
-
 console.log('The New File');
+function display() {
+alert("Welcome to the page! Take a short look!");
+}
 
 var x = 'a string variable';
 console.log(x);
 
 //console.log(document);
 //console.log($(document));
-console.log($('myId p'));
+console.log($('myId '));
 
 var mydiv = document.getElementById('myId');
 mydiv.setAttribute('style', 'color: blue');
+
 console.log(mydiv);
 //console.log($);
 //$( document ).ready(function() {
-  // Handler for .ready() called.
+ // Handler for .ready() called.
 //});
 //console.log('I am Ready to Zipline');
